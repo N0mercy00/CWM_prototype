900e5b55f7d6da4eff5e6a025c88fd9c536a4ad3
N0mercy00 <dkdhehesp@naver.com>
2021-08-27 17:52:27 +0900

    자잘한 아이콘 수정

--- app/src/main/java/com/dongdong999/cwm_layout/BlankFragment3.kt
+++ app/src/main/java/com/dongdong999/cwm_layout/BlankFragment3.kt
@@ -1,19 +1,47 @@
 package com.dongdong999.cwm_layout
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Message
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 
 class BlankFragment3 : Fragment() {
+    private lateinit var MainwebView: WebView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val webView = view?.findViewById<WebView>(R.id.webview_fragment3)
+            webView?.webViewClient = WebViewClient()
+            webView?.webChromeClient = WebChromeClient()
+
+            // 원하는 주소를 WebView에 연결
+            webView?.loadUrl("https://www.purmeecard.com/public.do?request=cardSelectForm")
+
+
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_blank3, container, false)
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
 
--- app/src/main/java/com/dongdong999/cwm_layout/LoginActivity.kt
+++ app/src/main/java/com/dongdong999/cwm_layout/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.dongdong999.cwm_layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLogin : Button = findViewById(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            var intent : Intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
