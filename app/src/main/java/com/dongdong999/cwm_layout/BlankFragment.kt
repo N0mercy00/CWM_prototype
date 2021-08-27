2e5459ba98ffdfbf769020b14992946c2aa841d8
N0mercy00 <dkdhehesp@naver.com>
2021-08-27 16:40:42 +0900

    현재위치 버그 일단 미룸 엑셀데이터 지도에 띄우기만

--- app/src/main/java/com/dongdong999/cwm_layout/BlankFragment.kt
+++ app/src/main/java/com/dongdong999/cwm_layout/BlankFragment.kt
@@ -3,21 +3,18 @@ package com.dongdong999.cwm_layout
 import android.content.Context
 import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
-import android.location.GpsStatus.create
 import android.location.Location
 import android.location.LocationManager
-import android.location.LocationRequest
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.*
-import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -25,10 +22,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.libraries.places.api.net.PlacesClient
-import com.google.android.material.badge.BadgeDrawable.create
-import java.net.URI.create
-import java.util.jar.Manifest
+import org.apache.poi.hssf.usermodel.HSSFCell
+import org.apache.poi.hssf.usermodel.HSSFRow
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.poifs.filesystem.POIFSFileSystem
+import java.io.InputStream
+import com.dongdong999.cwm_layout.Data as Data
 
 
 class BlankFragment : Fragment(), OnMapReadyCallback {
@@ -38,12 +37,15 @@ class BlankFragment : Fragment(), OnMapReadyCallback {
     private lateinit var btnCurrent: Button
     private var cameraPosition: CameraPosition? = null
 
+    var currentLocation : LatLng? = null
     var mLocationManager: LocationManager? = null
     var mLocationListener: android.location.LocationListener? = null
 
     lateinit var mContext: Context
 
     private lateinit var gMap : GoogleMap
+
+    var Excelitems: MutableList<Data> = mutableListOf()
 
 
     override fun onAttach(context: Context) {
@@ -62,21 +64,47 @@ class BlankFragment : Fragment(), OnMapReadyCallback {
         googlemap.onCreate(savedInstanceState)
         googlemap.getMapAsync(this)
 
+        readExel()
+
         return rootView
     }
 
     //지도 초기화
     override fun onMapReady(googleMap: GoogleMap) {
+        //마커
         val marker = LatLng(37.568291, 126.997780)
         googleMap.addMarker(MarkerOptions().position(marker).title("여기"))
         gMap=googleMap
         Log.d("TAG", "누르기전 googleMap 값은 ${googleMap.toString()} 들어간 지맵값은 ${gMap.toString()}")
 
+        if(currentLocation!=null){
+            cameraPosition = CameraPosition.builder().target(currentLocation)
+                .zoom(15.0F).build()
+            val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+            googleMap.moveCamera(cameraUpdate)
+        }else{
+            cameraPosition = CameraPosition.builder().target(LatLng(37.59215872,126.6759438))
+                .zoom(15.0F).build()
+            val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+            googleMap.moveCamera(cameraUpdate)
 
-        cameraPosition = CameraPosition.builder().target(LatLng(37.568291, 126.997780))
-            .zoom(15.0F).build()
-        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
-        googleMap.moveCamera(cameraUpdate)
+        }
+
+        //여기다가 엑셀 연동해서 마커찍자
+
+        readExel()
+        Log.d("TAG","${Excelitems[0].name}")
+
+        for(i in 0 until Excelitems.size){
+            var tempposition=LatLng(Excelitems[i].lon.toDouble(),Excelitems[i].lat.toDouble())
+            googleMap.addMarker(MarkerOptions().position(tempposition).title(Excelitems[i].name))
+        }
+
+
+
+
+
+
 
     }
 
@@ -84,7 +112,7 @@ class BlankFragment : Fragment(), OnMapReadyCallback {
     //이게 지도 만들어 지고 나서 하는 부분
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        var location2:LatLng?=null
+
 
         mLocationManager = mContext.getSystemService(LOCATION_SERVICE) as LocationManager
         mLocationListener = object : LocationListener, android.location.LocationListener {
@@ -99,9 +127,10 @@ class BlankFragment : Fragment(), OnMapReadyCallback {
                     lng = p0.longitude
                     Log.d("TAG", "Lat: ${lat}, lon: ${lng}")
                 }
-                var currentLocation = LatLng(lat, lng)
-                location2=currentLocation
-                gMap!!.addMarker(MarkerOptions().position(currentLocation).title("현재위치"))
+                currentLocation = LatLng(lat, lng)
+
+                //현재위치 마커찍기
+                //gMap!!.addMarker(MarkerOptions().position(currentLocation!!).title("현재위치"))
                 Log.d("TAG", "Lat: ${lat}, lon: ${lng}")
                 gMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 20f))
             }
@@ -119,15 +148,16 @@ class BlankFragment : Fragment(), OnMapReadyCallback {
             ) {
                 mLocationManager!!.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER,
-                    1000,
-                    30f,
+                    3000,
+                    10f,
                     mLocationListener!!
 
                 )
                 Log.d("TAG", "위치 새로고침")
 
-                /*googleMap2.addMarker(MarkerOptions().position(location2!!).title("현재위치"))
-                googleMap2.animateCamera(CameraUpdateFactory.newLatLngZoom(location2, 15f))*/
+                /*gMap.addMarker(MarkerOptions().position(location2!!).title("현재위치"))
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location2, 15f))
+                Log.d("TAG", "위치 새로고침2")*/
             }
 
         }
@@ -161,6 +191,69 @@ class BlankFragment : Fragment(), OnMapReadyCallback {
     override fun onDestroy() {
         googlemap.onDestroy()
         super.onDestroy()
+    }
+
+    fun readExel() {
+        try {
+            val myInput: InputStream
+            // assetManager 초기 설정
+            val assetManager = resources.assets
+            //  엑셀 시트 열기
+            myInput = assetManager.open("seogu.xls")
+            // POI File System 객체 만들기
+            val myFileSystem = POIFSFileSystem(myInput)
+            //워크 북
+            val myWorkBook = HSSFWorkbook(myFileSystem)
+            // 워크북에서 시트 가져오기
+            val sheet = myWorkBook.getSheetAt(0)
+
+            //행을 반복할 변수 만들어주기
+            val rowIter = sheet.rowIterator()
+            //행 넘버 변수 만들기
+            var rowno = 0
+            //MutableList 생성
+
+
+            //행 반복문
+            while (rowIter.hasNext()) {
+                val myRow = rowIter.next() as HSSFRow
+                if (rowno != 0) {
+                    //열을 반복할 변수 만들어주기
+                    val cellIter = myRow.cellIterator()
+                    //열 넘버 변수 만들기
+                    var colno = 0
+                    var name = ""
+                    var nmber = ""
+                    var lon =""
+                    var lat =""
+                    var phone =""
+                    var address =""
+                    //열 반복문
+                    while (cellIter.hasNext()) {
+                        val myCell = cellIter.next() as HSSFCell
+                        if (colno === 0) {
+                            name = myCell.toString()
+                        }else if (colno==5){
+                            address=myCell.toString()
+                        } else if (colno === 7) {
+                            lon = myCell.toString()
+                        }else if(colno==8){
+                            lat=myCell.toString()
+                        }else if(colno==9){
+                            phone=myCell.toString()
+                        }
+                        colno++
+                    }
+                    //4,8번째 열을 Mutablelist에 추가
+                    Excelitems.add(Data(name,phone,lon,lat,address))
+                }
+                rowno++
+            }
+            Log.e("TAG", " items: " + Excelitems)
+        } catch (e: Exception) {
+            Toast.makeText(activity, "에러 발생", Toast.LENGTH_LONG).show()
+        }
+
     }
 
 
---
+++ app/src/main/java/com/dongdong999/cwm_layout/Data.kt
@@ -0,0 +1,3 @@
+package com.dongdong999.cwm_layout
+
+data class Data (var name: String , var number:String, var lon : String ,var lat:String, var address :String)
