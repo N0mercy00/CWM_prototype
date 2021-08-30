e62d9626d295696a69c85629de0d388b44e47010
N0mercy00 <dkdhehesp@naver.com>
2021-08-30 09:50:23 +0900

    Login

--- app/src/main/java/com/dongdong999/cwm_layout/BlankFragment3.kt
+++ app/src/main/java/com/dongdong999/cwm_layout/BlankFragment3.kt
@@ -22,19 +22,6 @@ class BlankFragment3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val webView = view?.findViewById<WebView>(R.id.webview_fragment3)
-            webView?.webViewClient = WebViewClient()
-            webView?.webChromeClient = WebChromeClient()
-
-            // 원하는 주소를 WebView에 연결
-            webView?.loadUrl("https://www.purmeecard.com/public.do?request=cardSelectForm")
-
-
-
-
-
-
-
         return inflater.inflate(R.layout.fragment_blank3, container, false)
     }
 
@@ -42,6 +29,45 @@ class BlankFragment3 : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val webView = view?.findViewById<WebView>(R.id.webview_fragment3)
+        webView?.webViewClient = WebViewClient()
+        webView?.webChromeClient = WebChromeClient()
+
+        //웹뷰 설정
+        webView?.apply {
+            webViewClient = WebViewClientClass()
+            webChromeClient = object : WebChromeClient() {
+                override fun onCreateWindow(
+                    view: WebView?,
+                    isDialog: Boolean,
+                    isUserGesture: Boolean,
+                    resultMsg: Message?
+                ): Boolean {
+
+                    return true
+                }
+            }
+
+            //웹뷰 추가설정
+            settings.domStorageEnabled = true
+            settings.javaScriptEnabled = true                                      /*자바스크립트 허용 */
+            settings.setSupportMultipleWindows(false)                            /* 새창 띄우기 허용 */
+            settings.javaScriptCanOpenWindowsAutomatically = true             /*자바스크립트 새창 허용 */
+            settings.loadWithOverviewMode = true
+            settings.allowContentAccess = true;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                settings.safeBrowsingEnabled = true
+            }
+        }
+        
+        // 원하는 주소를 WebView에 연결
+        webView?.loadUrl("https://www.purmeecard.com/public.do?request=cardSelectForm")
     }
+}
+
+class WebViewClientClass : WebViewClient() {
+
 }
 
--- app/src/main/java/com/dongdong999/cwm_layout/LoginActivity.kt
+++ app/src/main/java/com/dongdong999/cwm_layout/LoginActivity.kt
@@ -1,20 +1,102 @@
 package com.dongdong999.cwm_layout
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.sign
 
 class LoginActivity : AppCompatActivity() {
+    private var auth : FirebaseAuth? = null
+
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth= Firebase.auth
+
         val btnLogin : Button = findViewById(R.id.btn_login)
+        var btnSignup : Button = findViewById(R.id.btn_signup)
+
+        var emailEditText=findViewById<EditText>(R.id.et_email)
+        var passwordEditText=findViewById<EditText>(R.id.et_password)
+
 
         btnLogin.setOnClickListener {
-            var intent : Intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+            /*var intent : Intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)*/
+
+            signIn(emailEditText.text.toString(),passwordEditText.text.toString())
+        }
+        btnSignup.setOnClickListener {
+            createAccount(emailEditText.text.toString(),passwordEditText.text.toString())
+        }
+
+    }
+
+    // 로그아웃하지 않을 시 자동 로그인 , 회원가입시 바로 로그인 됨
+    public override fun onStart() {
+        super.onStart()
+        moveMainPage(auth?.currentUser)
+    }
+
+    private fun createAccount(email:String, password:String){
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "계정 생성 완료.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    } else {
+                        Toast.makeText(
+                            this, "계정 생성 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                       // Log.d("TAG","ID : ${emailEditText.text.toString()} Password : ${passwordEditText.text.toString()}")
+                    }
+                }
+        }
+    }
+
+    private fun signIn(email: String,password: String){
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.signInWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext, "로그인에 성공 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        moveMainPage(auth?.currentUser)
+                    } else {
+                        Toast.makeText(
+                            baseContext, "로그인에 실패 하였습니다.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    fun moveMainPage(user: FirebaseUser?){
+        if( user!= null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
         }
     }
 }
